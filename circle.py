98837170a888cae5d986b329141a6e7676797dd6
Viktor Shubin <115558481+Lesbian-Catgirl@users.noreply.github.com>
2025-09-18 01:20:17 +0300

    Descriptions in circle.py
    Added descriptions for circle functions

--- circle.py
+++ circle.py
@@ -2,9 +2,27 @@ import math
 
 
 def area(r):
+    '''
+    Returns area of the circle
+
+    Parameters:
+        r: radius of the circle
+
+    Return value
+        : area of the circle
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Returns perimeter of the circle
+
+    Parameters:
+        r: radius of the circle
+
+    Return value
+        : perimeter of the circle
+    '''
     return 2 * math.pi * r
 
