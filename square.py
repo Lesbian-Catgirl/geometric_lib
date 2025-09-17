d4b5762abd9a6073f7b1729cf6d76992381180da
Viktor Shubin <115558481+Lesbian-Catgirl@users.noreply.github.com>
2025-09-18 01:12:39 +0300

    Descriprion to square.py
    Added descriptions to functions for square

--- square.py
+++ square.py
@@ -1,7 +1,17 @@
 
 def area(a):
+    ```
+    Returns area of the square
+
+    Parameters:
+        a: length of the square side
+
+    Return value
+        : area of the square
+    ```
     return a * a
 
 
 def perimeter(a):
+    ```Returns perimeter of the square
     return 4 * a
