87338f834ee5c09897f201e3c9bc2285f5fe499c
Viktor Shubin <115558481+Lesbian-Catgirl@users.noreply.github.com>
2025-09-18 01:18:28 +0300

    Description in rectangle.py
    Added descriptions for rectangle functions

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,25 @@
 def area(a, b):
+    '''
+    Returns area of the rectangle
+
+    Parameters:
+        a: length of one side of the rectangle
+        b: length of other side of the rectangle
+
+    Return value
+        : area of the rectangle
+    '''
     return a * b
 
 def perimeter(a, b):
+    '''
+    Returns perimeter of the rectangle
+
+    Parameters:
+        a: length of one side of the rectangle
+        b: length of other side of the rectangl
+
+    Return value
+        : perimeter of the rectangle
+    '''
     return 2 * (a + b)
